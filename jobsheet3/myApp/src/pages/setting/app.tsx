fa17704c2cd4be748c83612ec7f26d762f4b8543
Firmank123 <firmankediri123@gmail.com>
2026-02-24 08:40:37 +0700

    jobsheet3: Basic Nested Routing
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ jobsheet3/myApp/src/pages/setting/app.tsx
@@ -0,0 +1,10 @@
+const AppSetting = () => {
+    return (
+        <div>
+            <h1>App Settings Page</h1>
+            <p>Fireman app setting.</p>
+        </div>
+    );
+}
+
+export default AppSetting;
