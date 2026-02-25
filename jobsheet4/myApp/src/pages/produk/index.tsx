4a85c32174f5dcf556fb82ac44eda6286f9f3a34
Firmank123 <firmankediri123@gmail.com>
2026-02-25 14:28:30 +0700

    jobsheet4: Task 2
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- jobsheet4/myApp/src/pages/produk/index.tsx
+++ jobsheet4/myApp/src/pages/produk/index.tsx
@@ -1,20 +1,20 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect, use } from 'react';
+import { useRouter } from "next/router";
+import { useState, useEffect, use } from "react";
 
 const produk = () => {
-    const [isLogin, setIsLogin] = useState(false);
-    const { push } = useRouter();
-    useEffect(() => {
-        if (!isLogin) {
-            push('/auth/login');
-        }
-    }, []);
-    return (
-        <div>
-            <h1>Halaman Produk</h1>
-            <p>Daftar Fireman produk.</p>
-        </div>
-    );
-}
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    if (isLogin) {
+      push("/auth/login");
+    }
+  }, []);
+  return (
+    <div>
+      <h1>Halaman Produk</h1>
+      <p>Daftar Fireman produk.</p>
+    </div>
+  );
+};
 
 export default produk;
