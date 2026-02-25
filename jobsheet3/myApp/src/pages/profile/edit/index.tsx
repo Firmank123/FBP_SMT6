b907eccf976a09580e518b9e152a2bcc303956c3
Firmank123 <firmankediri123@gmail.com>
2026-02-25 11:48:45 +0700

    jobsheet3: Task 1 - Routing
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ jobsheet3/myApp/src/pages/profile/edit/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function EditProfilePage() {
+  return (
+    <div>
+      <h1>Edit Profile</h1>
+      <p>Edit your profile information here.</p>
+    </div>
+  );
+}
